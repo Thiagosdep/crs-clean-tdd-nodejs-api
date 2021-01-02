aab8f201b1681cf4d8684d6da7efc31f109c833f
Thiago Sousa de Paiva <thiago_sousap@hotmail.com>
2021-01-02 15:09:50 -0300

    ensure EmailValidator calls validator with correct email

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -6,17 +6,30 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return {
+    sut: new EmailValidator()
+  }
+}
+
 describe('Email validator', () => {
   test('Should return true if validator returns true', () => {
-    const sut = new EmailValidator()
-    const isEmailValid = sut.isValid('valid_email@email.com')
+    const { sut } = makeSut()
+    const isEmailValid = sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(true)
   })
 
   test('Should return false if validator returns false', () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
-    const isEmailValid = sut.isValid('invalid_email@email.com')
+    const { sut } = makeSut()
+    const isEmailValid = sut.isValid('invalid_email@mail.com')
     expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator with correct email', () => {
+    validator.isEmailValid = false
+    const { sut } = makeSut()
+    sut.isValid('any_email@mail.com')
+    expect(validator.email).toBe('any_email@mail.com')
   })
 })
