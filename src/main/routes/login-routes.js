3af45482054be71c031db5f4b98bbe8f37464d02
Thiago Sousa de Paiva <thiago_sousap@hotmail.com>
2021-01-06 00:20:39 -0300

    remove globalConfig from source

--- src/main/routes/login-routes.js
+++ src/main/routes/login-routes.js
@@ -1,2 +1,10 @@
+// const LoginRouter = require('../../presentation/routers/login-router')
+// const AuthUseCase = require('../../domain/usecases/auth-usecase')
+// const EmailValidator = require('../../utils/helpers/email-validator')
+
 module.exports = router => {
+//   const emailValidor = new EmailValidator()
+//   const authUseCase = new AuthUseCase()
+//   const loginRouter = new LoginRouter(authUseCase)
+//   router.post('/login', loginRouter)
 }
